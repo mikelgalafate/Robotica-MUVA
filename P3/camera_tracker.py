ad00528823b238069e49c2394535f3380f1afb14
Mikel <mikelgalafate15@gmail.com>
2023-05-18 00:17:59 +0200

    Update camera_tracker.py

--- P3/camera_tracker.py
+++ P3/camera_tracker.py
@@ -10,34 +10,41 @@ matplotlib.use("TkAgg")
 
 
 parser = argparse.ArgumentParser()
-parser.add_argument("-v", "--video", help="Path for the ", required=False)
+parser.add_argument("-v", "--video", help="Path for the detection video", required=False)
 parser.add_argument("-c", "--calibration", help="Path for the calibration images directory", required=False)
+parser.add_argument("-i", "--ip", help="URL for IP webcam", required=False)
 args = parser.parse_args()
 
+
 print("Camera calibration:")
-if args.video is None or args.calibration is None:
+if (args.video is None or args.calibration is None) and args.ip is None:
     if (args.video is None) ^ (args.calibration is None):
         print("Either both or none of the arguments must be specified")
         print("Would you like to continue with the default camera? [y/n]: ", end="")
+
         ans = input()
         if ans != 'y' and ans != 'Y':
             exit()
-    print()
-    args.video = 0
-    calib_images = utils.default_camera_calibration_images()
-    camera_values, num_valid_images = utils.calibrate_camera(calib_images)
-
+    else:
+        args.video = 0
+        print("Default camera will be used")
+    calib_images = utils.default_camera_calibration_images(args.video)
+elif args.ip is not None:
+    calib_images = utils.default_camera_calibration_images(args.ip)
+    args.video = args.ip
 else:
     print("\tReading calibration images")
     calib_images = [cv2.resize(cv2.imread(im), (1280, 720)) for im in tqdm(glob.glob(args.calibration + "/*"))]
-    camera_values, num_valid_images = utils.calibrate_camera(calib_images)
+
+camera_values, num_valid_images = utils.calibrate_camera(calib_images)
 
 while num_valid_images == 0:
     print("No valid images were found")
     print("Would you like to calibrate again with the default camera? [y/n]: ", end="")
     ans = input()
     if ans == 'y' or ans == 'Y':
-        calib_images = utils.default_camera_calibration_images()
+        args.video = 0
+        calib_images = utils.default_camera_calibration_images(args.video)
         camera_values, num_valid_images = utils.calibrate_camera(calib_images)
     elif ans == 'n' or ans == 'N':
         exit()
@@ -59,6 +66,10 @@ plt.figure()
 axes = plt.axes(projection="3d")
 
 centers = []
+axis_points = np.array([[0, 0, 0],
+                        [10, 0, 0],
+                        [0, 10, 0],
+                        [0, 0, 10]])
 
 cap = cv2.VideoCapture(args.video)
 while cap.isOpened():
@@ -70,7 +81,7 @@ while cap.isOpened():
         aruco_corners, ids, _ = cv2.aruco.detectMarkers(frame, aruco_dictionary, parameters=aruco_params)
         if aruco_corners:
             for i, tag_id in enumerate(ids):
-                print(f"Tag num.:{tag_id[0]} found", end="\r")
+                print(f"Tag num. {tag_id[0]} found", end="\r")
                 for corner in aruco_corners[i].squeeze().astype("int"):
                     cv2.circle(frame, corner, 2, (0, 255, 0), 5)
                 tag_corners = np.hstack((aruco_corners[i].squeeze(), np.zeros((4, 1)))).astype(np.float32)
@@ -79,7 +90,7 @@ while cap.isOpened():
                                                         tag_corners,
                                                         intrinsics,
                                                         dist_coeffs)
-                frame = cv2.drawFrameAxes(frame, intrinsics, dist_coeffs, rotation, translation, 80, thickness=2)
+                frame = cv2.drawFrameAxes(frame, intrinsics, dist_coeffs, rotation, translation, 100, thickness=2)
 
             R, _ = cv2.Rodrigues(rotation)
             C = -R.T @ translation
@@ -99,14 +110,17 @@ while cap.isOpened():
 
         else:
             print("No ArUco tag found", end="\r")
+
         cv2.imshow("Video", frame)
-        wk = cv2.waitKey(16)
+        wk = cv2.waitKey(24)
         if wk & 0xFF == ord('p'):
+            cv2.imwrite("img/axis.jpg", frame)
             wk = cv2.waitKey(0)
         if wk & 0xFF == ord('q'):
             cv2.destroyAllWindows()
             cap.release()
             plt.close()
+
     else:
         cap.release()
 
